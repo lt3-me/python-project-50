7bb140ab782a9b240a5126fb3670637298446598
Artur Medvedev <varouser@gmail.com>
2023-09-04 15:59:09 +0600

    Add yaml tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -3,9 +3,11 @@ from os import path
 
 file1 = path.join(path.dirname(__file__), 'fixtures', 'file1.json')
 file2 = path.join(path.dirname(__file__), 'fixtures', 'file2.json')
+file1p = path.join(path.dirname(__file__), 'fixtures', 'file1.yaml')
+file2p = path.join(path.dirname(__file__), 'fixtures', 'file2.yml')
 
 
-def test_diff():
+def test_diff_json():
     diff_result_file = path.join(path.dirname(__file__), 'fixtures',
                                  'test_diff_result.txt')
     with open(diff_result_file, "r") as f:
@@ -13,9 +15,17 @@ def test_diff():
     assert generate_diff(file1, file2) == result
 
 
-def test_diff_same():
+def test_diff_json_same():
     diff_same_result_file = path.join(path.dirname(__file__), 'fixtures',
                                       'test_same_result.txt')
     with open(diff_same_result_file, "r") as f:
         result = f.read()
-    assert generate_diff(file1, file1) == result
+    assert generate_diff(file1p, file1p) == result
+
+
+def test_diff_yaml():
+    diff_result_file = path.join(path.dirname(__file__), 'fixtures',
+                                 'test_diff_result.txt')
+    with open(diff_result_file, "r") as f:
+        result = f.read()
+    assert generate_diff(file1p, file2p) == result
