94fea1369a2c2b6bd811e87a3c6326d8df1df00e
Artur Medvedev <varouser@gmail.com>
2023-09-10 21:58:13 +0600

    Refactor json format func

--- gendiff/formats/json.py
+++ gendiff/formats/json.py
@@ -4,11 +4,13 @@ import json
 def format(dict1, dict2, diff):
     for key in diff:
         if diff[key] == 'added' or diff[key] == 'unchanged':
-            if isinstance(dict2[key], dict):
-                diff[key] = unpack_nested_with_status(dict2[key], diff[key])
+            dict_ = dict2
         elif diff[key] == 'removed':
-            if isinstance(dict1[key], dict):
-                diff[key] = unpack_nested_with_status(dict1[key], diff[key])
+            dict_ = dict1
+        else:
+            continue
+        if isinstance(dict_[key], dict):
+            diff[key] = unpack_nested_with_status(dict_[key], diff[key])
     json_string = json.dumps(diff)
     return json_string
 
