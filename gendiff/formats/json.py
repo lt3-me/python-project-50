a4a88bac9948fba0e644bfe0deda462a0485b8b6
Artur Medvedev <varouser@gmail.com>
2023-09-13 22:55:27 +0600

    Delete unused code

--- gendiff/formats/json.py
+++ gendiff/formats/json.py
@@ -4,13 +4,3 @@ import json
 def format(diff):
     json_string = json.dumps(diff)
     return json_string
-
-
-# def unpack_nested_with_status(dict_, status):
-#     unpacked = {}
-#     for key, value in dict_.items():
-#         if isinstance(value, dict):
-#             unpacked[key] = unpack_nested_with_status(value, status)
-#         else:
-#             unpacked[key] = status
-#     return unpacked
