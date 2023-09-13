04684b81f61c68f6b1d720cfc0bc884871fb9a1e
Artur Medvedev <varouser@gmail.com>
2023-09-13 23:10:40 +0600

    Fix over-indent

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,10 +3,9 @@ from gendiff import generate_diff
 
 
 def main():
-    parser = argparse.ArgumentParser(
-                    prog='gendiff',
-                    description='Compares two configuration files \
-                        and shows a difference.')
+    parser = argparse.ArgumentParser(prog='gendiff',
+                                     description='\
+Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', type=str, default='stylish',
