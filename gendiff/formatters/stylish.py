6790a94c0dcc333ce02514a607b0812eb12d3307
Artur Medvedev <varouser@gmail.com>
2023-11-06 19:22:45 +0600

    Fix _format()

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -2,15 +2,18 @@ INDENTATION = 2
 NEXT_LAYER_INDENT = 4
 
 
-def format_value(value):
-    value = 'true' if value is True else value
-    value = 'false' if value is False else value
-    value = 'null' if value is None else value
+def to_str(value):
+    if value is True:
+        return 'true'
+    if value is False:
+        return 'false'
+    if value is None:
+        return 'null'
     return value
 
 
 def format_single(key, value, prefix, indent):
-    formatted_value = format_value(value)
+    formatted_value = to_str(value)
     return f"{' ' * indent}{prefix} {key}: {formatted_value}\n"
 
 
@@ -36,12 +39,13 @@ def format_key_value(key, value, prefix, indent):
 
 
 def format(diff):
-    strings = []
-    output = '{\n' + ''.join(_format(diff, strings, INDENTATION)) + '}'
+    output = '{\n' + ''.join(_format(diff, INDENTATION)) + '}'
     return output
 
 
-def _format(diff, strings, indent):
+def _format(diff, indent):
+    strings = []
+
     for key in diff:
         if diff[key].get('children') is None:
             match diff[key].get('status'):
@@ -64,8 +68,7 @@ def _format(diff, strings, indent):
                     raise Exception('Diff formatting error')
         else:
             strings.append(f"{' ' * indent}  {key}: {{\n")
-            _format(diff[key].get('children'),
-                    strings,
-                    indent + NEXT_LAYER_INDENT)
+            strings.extend(_format(diff[key].get('children'),
+                           indent + NEXT_LAYER_INDENT))
             strings.append(f"{' ' * indent}  }}\n")
     return strings
