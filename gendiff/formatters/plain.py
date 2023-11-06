00b34dfe23b09fcbc11d3d04147478115733c192
Artur Medvedev <varouser@gmail.com>
2023-11-06 19:11:53 +0600

    Rename format_value() func to to_str()

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,4 +1,4 @@
-def format_value(value):
+def to_str(value):
     value = f"'{value}'" if isinstance(value, str) else value
     value = 'true' if value is True else value
     value = 'false' if value is False else value
@@ -21,15 +21,15 @@ def _format(diff, path):
                 case 'unchanged':
                     pass
                 case 'added':
-                    value = format_value(diff[key].get('value'))
+                    value = to_str(diff[key].get('value'))
                     strings.append(f"\
 Property '{path}{key}' was added with value: {value}\n")
                 case 'removed':
                     strings.append(f"\
 Property '{path}{key}' was removed\n")
                 case 'updated':
-                    value = format_value(diff[key].get('old_value'))
-                    value_new = format_value(diff[key].get('value'))
+                    value = to_str(diff[key].get('old_value'))
+                    value_new = to_str(diff[key].get('value'))
                     strings.append(f"\
 Property '{path}{key}' was updated. From {value} to {value_new}\n")
                 case _:
