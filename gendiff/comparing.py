63b37dbfcc65ec9ffb0c7b4985511752ec37e09b
Artur Medvedev <varouser@gmail.com>
2023-11-06 18:29:32 +0600

    Merge functions into one

--- gendiff/comparing.py
+++ gendiff/comparing.py
@@ -5,22 +5,20 @@ def get_difference(file1, file2):
     combined_keys_set = sorted(keys_set1.union(keys_set2))
 
     for key in combined_keys_set:
-        node = compare_values(file1.get(key, object),
-                              file2.get(key, object))
-        diff[key] = node
+        node1 = file1.get(key, object)
+        node2 = file2.get(key, object)
+
+        if node1 == node2:
+            diff[key] = {'status': 'unchanged', 'value': node1}
+        elif node1 is object:
+            diff[key] = {'status': 'added', 'value': node2}
+        elif node2 is object:
+            diff[key] = {'status': 'removed', 'value': node1}
+        elif isinstance(node1, dict) and isinstance(node2, dict):
+            diff[key] = {'status': 'updated',
+                         'children': get_difference(node1, node2)}
+        else:
+            diff[key] = {'status': 'updated',
+                         'value': node2, 'old_value': node1}
 
     return diff
-
-
-def compare_values(node1, node2):
-    if node1 == node2:
-        return {'status': 'unchanged', 'value': node1}
-    elif node1 is object:
-        return {'status': 'added', 'value': node2}
-    elif node2 is object:
-        return {'status': 'removed', 'value': node1}
-    elif isinstance(node1, dict) and isinstance(node2, dict):
-        return {'status': 'updated',
-                'children': get_difference(node1, node2)}
-    else:
-        return {'status': 'updated', 'value': node2, 'old_value': node1}
