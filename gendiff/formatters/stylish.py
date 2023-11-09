59dd9dfa3c044dd7b888ef14854b84aca05c0c68
Artur Medvedev <varouser@gmail.com>
2023-11-09 14:17:44 +0600

    Create a combined to_str func

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -2,19 +2,8 @@ SHORT_INDENT = 2
 LONG_INDENT = 4
 
 
-def to_str(value):
-    if value is True:
-        return 'true'
-    if value is False:
-        return 'false'
-    if value is None:
-        return 'null'
-    return value
-
-
 def format_single(key, value, prefix, indent):
-    formatted_value = to_str(value)
-    return f"{' ' * indent}{prefix} {key}: {formatted_value}\n"
+    return f"{' ' * indent}{prefix} {key}: {value}\n"
 
 
 def format_nested(parent, dict_, prefix, indent):
@@ -22,20 +11,20 @@ def format_nested(parent, dict_, prefix, indent):
     output += (f"{' ' * indent}{prefix} {parent}: {{\n")
     indent += LONG_INDENT
     for key in dict_:
-        if isinstance(dict_[key], dict):
-            output += format_nested(key, dict_[key], ' ', indent)
-        else:
-            output += format_single(key, dict_[key], ' ', indent)
+        output += to_str(key, dict_[key], ' ', indent)
     indent -= LONG_INDENT
     output += (f"{' ' * indent}  }}\n")
     return output
 
 
-def format_key_value(key, value, prefix, indent):
+def to_str(key, value, prefix, indent):
     if isinstance(value, dict):
         return format_nested(key, value, prefix, indent)
-    else:
-        return format_single(key, value, prefix, indent)
+    if isinstance(value, bool):
+        value = 'true' if value else 'false'
+    if value is None:
+        value = 'null'
+    return format_single(key, value, prefix, indent)
 
 
 def format(diff):
@@ -50,20 +39,19 @@ def _format(diff, indent):
         if diff[key].get('children') is None:
             match diff[key].get('status'):
                 case 'added':
-                    strings.append(format_key_value(key, diff[key].get('value'),
-                                                    '+', indent))
+                    strings.append(to_str(key, diff[key].get('value'),
+                                          '+', indent))
                 case 'removed':
-                    strings.append(format_key_value(key, diff[key].get('value'),
-                                                    '-', indent))
+                    strings.append(to_str(key, diff[key].get('value'),
+                                          '-', indent))
                 case 'unchanged':
-                    strings.append(format_key_value(key, diff[key].get('value'),
-                                                    ' ', indent))
+                    strings.append(to_str(key, diff[key].get('value'),
+                                          ' ', indent))
                 case 'updated':
-                    strings.append(format_key_value(key,
-                                                    diff[key].get('old_value'),
-                                                    '-', indent))
-                    strings.append(format_key_value(key, diff[key].get('value'),
-                                                    '+', indent))
+                    strings.append(to_str(key, diff[key].get('old_value'),
+                                          '-', indent))
+                    strings.append(to_str(key, diff[key].get('value'),
+                                          '+', indent))
                 case _:
                     raise Exception('Diff formatting error')
         else:
