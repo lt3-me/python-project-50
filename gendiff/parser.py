3231e79a11bc0fcc0923fb313ac16884ba0eca4f
Artur Medvedev <varouser@gmail.com>
2023-11-09 13:50:58 +0600

    Fix parsing func

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -4,13 +4,13 @@ import yaml
 
 def get_dict_from_file(file_path):
     format = file_path.split('.')[-1]
-    dict_ = parse_content_from_file(file_path, format)
+    dict_ = parse_content(open(file_path), format)
     return dict_
 
 
-def parse_content_from_file(file, format):
+def parse_content(content, format):
     match format:
-        case 'json': content = json.load(open(file))
-        case 'yaml' | 'yml': content = yaml.safe_load(open(file))
+        case 'json': content = json.load(content)
+        case 'yaml' | 'yml': content = yaml.safe_load(content)
         case _: raise Exception('Unsupported file format')
     return content
