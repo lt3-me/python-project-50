507f68c0f5bb6b865e6492db8a7b0e1bd8a3f1a9
Artur Medvedev <varouser@gmail.com>
2023-11-09 13:55:45 +0600

    Replace get_format_function w/ get_formatted_diff

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,12 +1,11 @@
 from gendiff.parser import get_dict_from_file
 from gendiff.comparer import get_difference
-from gendiff.formatters import get_format_function_from_str
+from gendiff.formatters import get_formatted_diff
 
 
 def generate_diff(file_path1, file_path2, format='stylish'):
     file1 = get_dict_from_file(file_path1)
     file2 = get_dict_from_file(file_path2)
-    format_func = get_format_function_from_str(format)
-
     diff = get_difference(file1, file2)
-    return format_func(diff)
+
+    return get_formatted_diff(diff, format)
--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -3,14 +3,14 @@ import gendiff.formatters.plain as plain
 import gendiff.formatters.json as json_formatter
 
 
-def get_format_function_from_str(str_format):
+def get_formatted_diff(diff, str_format):
     match str_format:
         case 'stylish':
-            format_func = stylish.format
+            formatted_diff = stylish.format(diff)
         case 'plain':
-            format_func = plain.format
+            formatted_diff = plain.format(diff)
         case 'json':
-            format_func = json_formatter.format
+            formatted_diff = json_formatter.format(diff)
         case _:
             raise Exception('Invalid format')
-    return format_func
+    return formatted_diff
