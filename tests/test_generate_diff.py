10552253b6bc47d22144569c2664a68713e648c4
Artur Medvedev <varouser@gmail.com>
2023-11-10 20:48:51 +0600

    Modify test func to read result files in its body

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -3,50 +3,32 @@ import pytest
 from os import path
 
 
+file_name_inputs = [('file1.json', 'file2.json'), ('file1.yaml', 'file2.yml')]
+styles = ('stylish', 'plain', 'json')
+results_diff = ('test_diff_result_stylish.txt',
+                'test_diff_result_plain.txt',
+                'test_diff_result_json.txt')
+results_same = ('test_same_result_stylish.txt',
+                'test_same_result_plain.txt',
+                'test_same_result_json.txt')
+parameters_diff = [(i1, i2, result_filename, style)
+                   for i1, i2 in file_name_inputs
+                   for style, result_filename in zip(styles, results_diff)]
+parameters_same = [(i1, i1, result_filename, style)
+                   for i1, _ in file_name_inputs
+                   for style, result_filename in zip(styles, results_same)]
+parameters = parameters_diff + parameters_same
+
+
 def get_fixture_filepath(filename):
     return path.join(path.dirname(__file__), 'fixtures', filename)
 
 
-def get_results(style):
-    if style == 'stylish':
-        diff_result_file_stylish = get_fixture_filepath(
-            'test_diff_result_stylish.txt')
-        diff_same_result_file_stylish = get_fixture_filepath(
-            'test_same_result_stylish.txt')
-        with open(diff_result_file_stylish, "r") as f:
-            result = f.read()
-        with open(diff_same_result_file_stylish, "r") as f:
-            result_same = f.read()
-    elif style == 'plain':
-        diff_result_file_plain = get_fixture_filepath(
-            'test_diff_result_plain.txt')
-        with open(diff_result_file_plain, "r") as f:
-            result = f.read()
-        result_same = ''
-    elif style == 'json':
-        diff_result_file_json = get_fixture_filepath(
-            'test_diff_result_json.txt')
-        diff_same_result_file_json = get_fixture_filepath(
-            'test_same_result_json.txt')
-        with open(diff_result_file_json, "r") as f:
-            result = f.read()
-        with open(diff_same_result_file_json, "r") as f:
-            result_same = f.read()
-    else:
-        raise Exception('Wrong formatting style')
-    return result, result_same
-
-
-file_name_inputs = [('file1.json', 'file2.json'), ('file1.yaml', 'file2.yml')]
-styles = ('stylish', 'plain', 'json')
-
-
 @pytest.mark.parametrize(
-    "input1, input2, style",
-    [(i1, i2, style) for i1, i2 in file_name_inputs for style in styles])
-def test_diff(input1, input2, style):
+    "input1, input2, expected_filename, style", parameters)
+def test_diff(input1, input2, expected_filename, style):
     file1 = get_fixture_filepath(input1)
     file2 = get_fixture_filepath(input2)
-    expected, expected_same = get_results(style)
+    with open(get_fixture_filepath(expected_filename), "r") as f:
+        expected = f.read()
     assert generate_diff(file1, file2, style) == expected
-    assert generate_diff(file1, file1, style) == expected_same
