7991abb6c46d5804def64d82c622b2a1f1d26301
Artur Medvedev <varouser@gmail.com>
2023-11-13 16:35:00 +0600

    Replace os.path.split with os.path.splitext

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -4,14 +4,14 @@ import os
 
 
 def get_dict_from_file(file_path):
-    format = os.path.split('.')[-1]
-    dict_ = parse_content(open(file_path), format)
-    return dict_
+    format = os.path.splitext(file_path)[-1]
+    with open(file_path, "r") as f:
+        return parse_content(f, format)
 
 
 def parse_content(content, format):
     match format:
-        case 'json': content = json.load(content)
-        case 'yaml' | 'yml': content = yaml.safe_load(content)
+        case '.json': content = json.load(content)
+        case '.yaml' | '.yml': content = yaml.safe_load(content)
         case _: raise Exception('Unsupported file format')
     return content
