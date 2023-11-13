2b60b8bf6115363f106e4107233a30b7bafd1b6c
Artur Medvedev <varouser@gmail.com>
2023-11-13 16:39:38 +0600

    Refactor get_difference to work in one cycle

--- gendiff/comparer.py
+++ gendiff/comparer.py
@@ -5,22 +5,20 @@ def get_difference(dict1, dict2):
 
     keys_added = keys_set2 - keys_set1
     keys_removed = keys_set1 - keys_set2
-    keys_in_both_sets = keys_set1.intersection(keys_set2)
+    all_keys = keys_set1.union(keys_set2)
 
-    for key in keys_added:
-        node = dict2.get(key, object)
-        diff[key] = {'status': 'added', 'value': node}
-
-    for key in keys_removed:
-        node = dict1.get(key, object)
-        diff[key] = {'status': 'removed', 'value': node}
-
-    for key in keys_in_both_sets:
+    for key in all_keys:
         node1 = dict1.get(key, object)
         node2 = dict2.get(key, object)
 
         if node1 == node2:
             diff[key] = {'status': 'unchanged', 'value': node1}
+        elif key in keys_added:
+            node = dict2.get(key, object)
+            diff[key] = {'status': 'added', 'value': node}
+        elif key in keys_removed:
+            node = dict1.get(key, object)
+            diff[key] = {'status': 'removed', 'value': node}
         elif isinstance(node1, dict) and isinstance(node2, dict):
             diff[key] = {'status': 'nested',
                          'children': get_difference(node1, node2)}
